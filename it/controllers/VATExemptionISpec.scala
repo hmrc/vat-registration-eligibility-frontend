e09d17e61a4b80f16ca6e3ed477a0b048e594800
Kaizar Laxmidhar <kaizar.laxmidhar@digital.hmrc.gov.uk>
2022-02-02 16:02:12 +0000

    SAR-9078 Add feature switch and when its enabled update exception and exemption pages to allow continuing after choosing yes

--- it/controllers/VATExemptionISpec.scala
+++ it/controllers/VATExemptionISpec.scala
@@ -1,12 +1,13 @@
 package controllers
 
+import config.FrontendAppConfig
+import featureswitch.core.config.ExceptionExemptionFlow
 import helpers.{AuthHelper, IntegrationSpecBase, SessionStub}
 import identifiers.{ThresholdInTwelveMonthsId, ThresholdNextThirtyDaysId}
 import models.ConditionalDateFormElement
 import play.api.Application
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.mvc.Http.HeaderNames
-import config.FrontendAppConfig
 
 class VATExemptionISpec extends IntegrationSpecBase with AuthHelper with SessionStub {
 
@@ -18,39 +19,78 @@ class VATExemptionISpec extends IntegrationSpecBase with AuthHelper with Session
 
   class Setup extends SessionTest(app)
 
-  s"${controllers.routes.VATExemptionController.onSubmit()}" should {
-    s"redirect the user to ${config.otrsUrl} when answer is true and MTD is mandatory" in new Setup {
-      stubSuccessfulLogin()
-      stubSuccessfulRegIdGet()
-      stubAudits()
+  s"${controllers.routes.VATExemptionController.onSubmit()}" when {
+    "ExceptionExemption flow feature switch is not enabled" should {
+      s"redirect the user to ${config.otrsUrl} when answer is true and MTD is mandatory" in new Setup {
+        stubSuccessfulLogin()
+        stubSuccessfulRegIdGet()
+        stubAudits()
 
-      cacheSessionData[ConditionalDateFormElement](sessionId, s"$ThresholdInTwelveMonthsId", ConditionalDateFormElement(true, None))
-      cacheSessionData[ConditionalDateFormElement](sessionId, s"$ThresholdNextThirtyDaysId", ConditionalDateFormElement(false, None))
+        cacheSessionData[ConditionalDateFormElement](sessionId, s"$ThresholdInTwelveMonthsId", ConditionalDateFormElement(true, None))
+        cacheSessionData[ConditionalDateFormElement](sessionId, s"$ThresholdNextThirtyDaysId", ConditionalDateFormElement(false, None))
 
-      val request = buildClient("/vat-exemption").withHttpHeaders(HeaderNames.COOKIE -> getSessionCookie(), "Csrf-Token" -> "nocheck")
-        .post(Map(
-          "value" -> Seq("true")
-        ))
-      val response = await(request)
-      response.status mustBe 303
-      response.header(HeaderNames.LOCATION) mustBe Some(("/check-if-you-can-register-for-vat/cant-register/OTRS"))
+        val request = buildClient("/vat-exemption").withHttpHeaders(HeaderNames.COOKIE -> getSessionCookie(), "Csrf-Token" -> "nocheck")
+          .post(Map(
+            "value" -> Seq("true")
+          ))
+        val response = await(request)
+        response.status mustBe 303
+        response.header(HeaderNames.LOCATION) mustBe Some(("/check-if-you-can-register-for-vat/cant-register/OTRS"))
+      }
+      s"redirect the user to ${controllers.routes.MandatoryInformationController.onPageLoad} when answer is false and MTD is mandatory" in new Setup {
+        stubSuccessfulLogin()
+        stubSuccessfulRegIdGet()
+        stubAudits()
+
+        cacheSessionData[ConditionalDateFormElement](sessionId, s"$ThresholdInTwelveMonthsId", ConditionalDateFormElement(true, None))
+        cacheSessionData[ConditionalDateFormElement](sessionId, s"$ThresholdNextThirtyDaysId", ConditionalDateFormElement(false, None))
+
+        val request = buildClient("/vat-exemption").withHttpHeaders(HeaderNames.COOKIE -> getSessionCookie(), "Csrf-Token" -> "nocheck")
+          .post(Map(
+            "value" -> Seq("false")
+          ))
+        val response = await(request)
+        response.status mustBe 303
+        response.header(HeaderNames.LOCATION) mustBe Some(controllers.routes.MandatoryInformationController.onPageLoad.url)
+      }
     }
-    s"redirect the user to ${controllers.routes.MandatoryInformationController.onPageLoad} when answer is false and MTD is mandatory" in new Setup {
-      stubSuccessfulLogin()
-      stubSuccessfulRegIdGet()
-      stubAudits()
+    "ExceptionExemption flow feature switch is enabled" should {
+      s"redirect the user to ${controllers.routes.MandatoryInformationController.onPageLoad} when answer is true and MTD is mandatory" in new Setup {
+        enable(ExceptionExemptionFlow)
+        stubSuccessfulLogin()
+        stubSuccessfulRegIdGet()
+        stubAudits()
 
-      cacheSessionData[ConditionalDateFormElement](sessionId, s"$ThresholdInTwelveMonthsId", ConditionalDateFormElement(true, None))
-      cacheSessionData[ConditionalDateFormElement](sessionId, s"$ThresholdNextThirtyDaysId", ConditionalDateFormElement(false, None))
+        cacheSessionData[ConditionalDateFormElement](sessionId, s"$ThresholdInTwelveMonthsId", ConditionalDateFormElement(true, None))
+        cacheSessionData[ConditionalDateFormElement](sessionId, s"$ThresholdNextThirtyDaysId", ConditionalDateFormElement(false, None))
 
-      val request = buildClient("/vat-exemption").withHttpHeaders(HeaderNames.COOKIE -> getSessionCookie(), "Csrf-Token" -> "nocheck")
-        .post(Map(
-          "value" -> Seq("false")
-        ))
-      val response = await(request)
-      response.status mustBe 303
-      response.header(HeaderNames.LOCATION) mustBe Some(controllers.routes.MandatoryInformationController.onPageLoad.url)
+        val request = buildClient("/vat-exemption").withHttpHeaders(HeaderNames.COOKIE -> getSessionCookie(), "Csrf-Token" -> "nocheck")
+          .post(Map(
+            "value" -> Seq("true")
+          ))
+        val response = await(request)
+        response.status mustBe 303
+        response.header(HeaderNames.LOCATION) mustBe Some(controllers.routes.MandatoryInformationController.onPageLoad.url)
+      }
+      s"redirect the user to ${controllers.routes.MandatoryInformationController.onPageLoad} when answer is false and MTD is mandatory" in new Setup {
+        enable(ExceptionExemptionFlow)
+        stubSuccessfulLogin()
+        stubSuccessfulRegIdGet()
+        stubAudits()
+
+        cacheSessionData[ConditionalDateFormElement](sessionId, s"$ThresholdInTwelveMonthsId", ConditionalDateFormElement(true, None))
+        cacheSessionData[ConditionalDateFormElement](sessionId, s"$ThresholdNextThirtyDaysId", ConditionalDateFormElement(false, None))
+
+        val request = buildClient("/vat-exemption").withHttpHeaders(HeaderNames.COOKIE -> getSessionCookie(), "Csrf-Token" -> "nocheck")
+          .post(Map(
+            "value" -> Seq("false")
+          ))
+        val response = await(request)
+        response.status mustBe 303
+        response.header(HeaderNames.LOCATION) mustBe Some(controllers.routes.MandatoryInformationController.onPageLoad.url)
+      }
     }
+
   }
 
 }
--- it/controllers/VATRegistrationExceptionISpec.scala
+++ it/controllers/VATRegistrationExceptionISpec.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import featureswitch.core.config.ExceptionExemptionFlow
 import helpers.{AuthHelper, IntegrationSpecBase, SessionStub}
 import play.api.Application
 import play.api.inject.guice.GuiceApplicationBuilder
@@ -13,32 +14,64 @@ class VATRegistrationExceptionISpec extends IntegrationSpecBase with AuthHelper 
 
   class Setup extends SessionTest(app)
 
-  s"${controllers.routes.VATRegistrationExceptionController.onSubmit()}" should {
-    s"redirect to ${"/check-if-you-can-register-for-vat/cant-register/vatExceptionKickout"} if answer is yes" in new Setup {
-      stubSuccessfulLogin()
-      stubSuccessfulRegIdGet()
-      stubAudits()
+  s"${controllers.routes.VATRegistrationExceptionController.onSubmit()}" when {
+    "ExceptionExemption flow feature switch is not enabled" should {
+      s"redirect to ${"/check-if-you-can-register-for-vat/cant-register/vatExceptionKickout"} if answer is yes" in new Setup {
+        stubSuccessfulLogin()
+        stubSuccessfulRegIdGet()
+        stubAudits()
 
-      val request = buildClient("/registration-exception").withHttpHeaders(HeaderNames.COOKIE -> getSessionCookie(), "Csrf-Token" -> "nocheck")
-        .post(Map(
-          "value" -> Seq("true"))
-        )
-      val response = await(request)
-      response.status mustBe 303
-      response.header(HeaderNames.LOCATION) mustBe Some("/check-if-you-can-register-for-vat/cant-register/vatExceptionKickout")
+        val request = buildClient("/registration-exception").withHttpHeaders(HeaderNames.COOKIE -> getSessionCookie(), "Csrf-Token" -> "nocheck")
+          .post(Map(
+            "value" -> Seq("true"))
+          )
+        val response = await(request)
+        response.status mustBe 303
+        response.header(HeaderNames.LOCATION) mustBe Some("/check-if-you-can-register-for-vat/cant-register/vatExceptionKickout")
+      }
+      s"redirect to ${controllers.routes.TurnoverEstimateController.onPageLoad} if answer is no" in new Setup {
+        stubSuccessfulLogin()
+        stubSuccessfulRegIdGet()
+        stubAudits()
+
+        val request = buildClient("/registration-exception").withHttpHeaders(HeaderNames.COOKIE -> getSessionCookie(), "Csrf-Token" -> "nocheck")
+          .post(Map(
+            "value" -> Seq("false"))
+          )
+        val response = await(request)
+        response.status mustBe 303
+        response.header(HeaderNames.LOCATION) mustBe Some(controllers.routes.TurnoverEstimateController.onPageLoad.url)
+      }
     }
-    s"redirect to ${controllers.routes.TurnoverEstimateController.onPageLoad} if answer is false" in new Setup {
-      stubSuccessfulLogin()
-      stubSuccessfulRegIdGet()
-      stubAudits()
+    "ExceptionExemption flow feature switch is enabled" should {
+      s"redirect to ${controllers.routes.TurnoverEstimateController.onPageLoad} if answer is yes" in new Setup {
+        enable(ExceptionExemptionFlow)
+        stubSuccessfulLogin()
+        stubSuccessfulRegIdGet()
+        stubAudits()
 
-      val request = buildClient("/registration-exception").withHttpHeaders(HeaderNames.COOKIE -> getSessionCookie(), "Csrf-Token" -> "nocheck")
-        .post(Map(
-          "value" -> Seq("false"))
-        )
-      val response = await(request)
-      response.status mustBe 303
-      response.header(HeaderNames.LOCATION) mustBe Some(controllers.routes.TurnoverEstimateController.onPageLoad.url)
+        val request = buildClient("/registration-exception").withHttpHeaders(HeaderNames.COOKIE -> getSessionCookie(), "Csrf-Token" -> "nocheck")
+          .post(Map(
+            "value" -> Seq("true"))
+          )
+        val response = await(request)
+        response.status mustBe 303
+        response.header(HeaderNames.LOCATION) mustBe Some(controllers.routes.TurnoverEstimateController.onPageLoad.url)
+      }
+      s"redirect to ${controllers.routes.TurnoverEstimateController.onPageLoad} if answer is no" in new Setup {
+        enable(ExceptionExemptionFlow)
+        stubSuccessfulLogin()
+        stubSuccessfulRegIdGet()
+        stubAudits()
+
+        val request = buildClient("/registration-exception").withHttpHeaders(HeaderNames.COOKIE -> getSessionCookie(), "Csrf-Token" -> "nocheck")
+          .post(Map(
+            "value" -> Seq("false"))
+          )
+        val response = await(request)
+        response.status mustBe 303
+        response.header(HeaderNames.LOCATION) mustBe Some(controllers.routes.TurnoverEstimateController.onPageLoad.url)
+      }
     }
   }
 }
