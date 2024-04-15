db25727566ecf0acbe3af6ead8e5cb211dfac706
baha.aboukoura <baha.aboukoura@digital.hmrc.gov.uk>
2024-04-15 15:33:23 +0100

    use local date time

--- app/repositories/SessionRepository.scala
+++ app/repositories/SessionRepository.scala
@@ -30,14 +30,14 @@ import uk.gov.hmrc.mongo.play.json.PlayMongoRepository
 import uk.gov.hmrc.mongo.play.json.formats.MongoJavatimeFormats
 import uk.gov.hmrc.play.http.logging.Mdc
 
-import java.time.Instant
+import java.time.{LocalDate, LocalDateTime, ZoneOffset, ZonedDateTime}
 import java.util.concurrent.TimeUnit
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 
 case class DatedCacheMap(id: String,
                          data: Map[String, JsValue],
-                         lastUpdated: Instant = Instant.now()) {
+                         lastUpdated: LocalDateTime = ZonedDateTime.now(ZoneOffset.UTC).toLocalDateTime) {
   def as[T](implicit f: DatedCacheMap => T): T = f(this)
 }
 
@@ -69,7 +69,7 @@ class SessionRepository @Inject()(config: Configuration,
     )
   ) {
 
-  implicit val dateFormat: Format[Instant] = MongoJavatimeFormats.instantFormat
+  implicit val dateFormat: Format[LocalDate] = MongoJavatimeFormats.localDateFormat
 
   def get(id: String): Future[Option[CacheMap]] = Mdc.preservingMdc {
     collection.find(equal("id", id)).map(_.as[CacheMap]).headOption()
