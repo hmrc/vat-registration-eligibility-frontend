a5dd659949e2ac1bb4627d17291372cc73645046
kannanekambaram <112036677+kannanEkambaram@users.noreply.github.com>
2025-05-15 14:05:51 +0100

    Revert "fix for DL-16238 (#365)"
    This reverts commit 9676a896bf372fb5d69d2625aa358aaf8a69216c.

--- app/assets/javascripts/backLink.js
+++ app/assets/javascripts/backLink.js
@@ -3,8 +3,6 @@ var backLink = document.querySelector('.govuk-back-link[href="#"]')
 if (backLink != null) {
     backLink.addEventListener('click', function(e) {
         e.preventDefault();
-        if (!backLink.hasAttribute('data-clicked')) {
-                    backLink.setAttribute('data-clicked', 'true');
         window.history.back();
     })
 }
